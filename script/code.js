aa0e74946968c0d1454d5cc549f6fe2a1d3cea4c
Taflnne15 <taflynnepetersen@gmail.com>
2023-05-31 14:49:23 +0200

    complete

---
+++ script/code.js
@@ -0,0 +1,25 @@
+
+
+//first way
+let first_number = 20;
+let last_number = 21;
+
+[first_number, second_number] = [last_number, first_number] //swapping the property of the value
+console.log(first_number); //20
+console.log(second_number); //21
+
+//sceond way
+let firstnumber = 20;
+let lastnumber = 21;
+let swap = [lastnumber , firstnumber] // made an array of the numbers swap
+console.log(swap)
+
+//third way 
+let first = 21;
+let second = 20;
+let numbers = ['20', '21'];
+console.log(numbers);
+
+
+
+
